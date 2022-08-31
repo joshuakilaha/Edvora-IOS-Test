24f19e07a15a2b1988fe96563d095e7f2b4422ce
joshua kilaha <jokilaha191@gmail.com>
2022-08-31 15:25:33 +0300

    Initial Commit

---
+++ Edvora IOS Test/Edvora_IOS_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Edvora_IOS_TestApp.swift
+//  Edvora IOS Test
+//
+//  Created by Mroot on 31/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Edvora_IOS_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
