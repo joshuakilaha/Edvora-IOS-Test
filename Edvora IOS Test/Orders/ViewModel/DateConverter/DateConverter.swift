792de82af2d7627ad0426df4fed5c8a16d885bbe
joshua kilaha <jokilaha191@gmail.com>
2022-08-31 21:52:50 +0300

    Date Converter Init

---
+++ Edvora IOS Test/Orders/ViewModel/DateConverter/DateConverter.swift
@@ -0,0 +1,18 @@
+//
+//  DateConverter.swift
+//  Edvora IOS Test
+//
+//  Created by Mroot on 31/08/2022.
+//
+
+import Foundation
+
+class DateConverter {
+    // MARK: Date and Time -> 31 Aug, 8:00 am
+    func dateOrdered(_ date: String) -> String {
+        let timeStamp = Date(timeIntervalSince1970: TimeInterval(date)!)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "d MMM, h:mm a"
+        return dateFormatter.string(from: timeStamp)
+    }
+}
