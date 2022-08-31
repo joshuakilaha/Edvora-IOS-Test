8026b612435650f8c3060341090def5853a60cf6
joshua kilaha <jokilaha191@gmail.com>
2022-08-31 17:46:47 +0300

    SF Tab Symbols

---
+++ Edvora IOS Test/Resources/Symbols.swift
@@ -0,0 +1,15 @@
+//
+//  Symbols.swift
+//  Edvora IOS Test
+//
+//  Created by Mroot on 31/08/2022.
+//
+
+import SwiftUI
+
+enum Symbols {
+    static let home = Image(systemName: "house")
+    static let orders = Image(systemName: "cart")
+    static let product = Image(systemName: "shippingbox")
+    static let user = Image(systemName: "person")
+}
