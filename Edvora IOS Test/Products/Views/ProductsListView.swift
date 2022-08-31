5deecfef5debd7e8467bbcbf6b50e2396f67de65
joshua kilaha <jokilaha191@gmail.com>
2022-08-31 17:46:56 +0300

    Views Init

---
+++ Edvora IOS Test/Products/Views/ProductsListView.swift
@@ -0,0 +1,20 @@
+//
+//  ProductsListView.swift
+//  Edvora IOS Test
+//
+//  Created by Mroot on 31/08/2022.
+//
+
+import SwiftUI
+
+struct ProductsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProductsListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductsListView()
+    }
+}
---
+++ Edvora IOS Test/Users/Views/UsersListView.swift
@@ -0,0 +1,20 @@
+//
+//  UsersListView.swift
+//  Edvora IOS Test
+//
+//  Created by Mroot on 31/08/2022.
+//
+
+import SwiftUI
+
+struct UsersListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UsersListView_Previews: PreviewProvider {
+    static var previews: some View {
+        UsersListView()
+    }
+}
