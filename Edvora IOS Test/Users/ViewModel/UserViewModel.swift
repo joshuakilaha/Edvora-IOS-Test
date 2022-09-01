0d21e889b29501f4c97fa84d7e97f06376e6dfdd
joshua kilaha <jokilaha191@gmail.com>
2022-09-01 14:10:14 +0300

    Fetch from network

---
+++ Edvora IOS Test/Users/ViewModel/UserViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  UserViewModel.swift
+//  Edvora IOS Test
+//
+//  Created by Mroot on 01/09/2022.
+//
+
+import Foundation
+
+final class UserViewModel: ObservableObject {
+    @Published private(set) var users = [User]()
+    @Published private(set) var error: NetworkManager.NetworkError?
+    @Published var hasError = false
+    @Published var isLoading = false
+    
+    func getUsers() async {
+     isLoading = true
+        do {
+            let usersDecoded = try await NetworkManager.shared.fetchRequest(API.baseURL + API.users, T: [User].self)
+            DispatchQueue.main.async {
+                defer { self.isLoading = false }
+                self.users = usersDecoded
+            }
+        } catch {
+            hasError = true
+            if let networkError = error as? NetworkManager.NetworkError {
+                self.error = networkError
+            } else {
+                self.error = .customError(error: error)
+            }
+        }
+    }
+}
