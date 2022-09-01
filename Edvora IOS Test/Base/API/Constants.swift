4bf021ee329f7a97e3ff3862d7def0c34a00ec19
joshua kilaha <jokilaha191@gmail.com>
2022-09-01 14:09:14 +0300

    API Constant txt

---
+++ Edvora IOS Test/Base/API/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Edvora IOS Test
+//
+//  Created by Mroot on 01/09/2022.
+//
+
+import Foundation
+
+enum API {
+    static let baseURL = "https://assessment.api.vweb.app"
+    static let orders = "/orders"
+    static let products = "/products"
+    static let users = "/users"
+
+}
